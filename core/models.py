b28af73469ba09aae23e464a26f2be4ef30c5f3e
PanchiXxL <molinaedison5@gmail.com>
2021-03-15 23:52:48 -0500

    hola

--- core/models.py
+++ core/models.py
@@ -13,7 +13,7 @@ class Type(models.Model):
         db_table='Tipo'
         ordering=['id']
 
-
+print("hola")
 class Empleado(models.Model):
     type=models.ForeignKey(Type, on_delete=models.CASCADE)
     nombre=models.CharField(max_length=30, verbose_name="Nombre: ")
